890c2285616d24f23a64a3428c78b50d8aa98a1a
Junyan Qin <1010553892@qq.com>
2023-12-25 05:59:45 +0000

    feat: 允许指定运行的测试用例目录

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import logging
+import os
+
 import pytest
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    pytest.main(["src"])
+
+    test_dirs = "src"
+    if 'TEST_DIRS' in os.environ:
+        test_dirs = os.environ['TEST_DIRS']
+    dirs = test_dirs.split(':')
+
+    print(f"Running tests in {dirs}")
+    
+    pytest.main(dirs)
