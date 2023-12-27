1a5df11407dc5e7afcacd44383fe757b743a43e4
Junyan Qin <1010553892@qq.com>
2023-12-27 14:51:57 +0000

    feat: 启动时打印所有环境变量

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import pytest
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", handlers=[logging.StreamHandler()])
 
+    # 打印所有环境变量
+    for key, value in os.environ.items():
+        print(f"env: {key}={value}")
+
     test_dirs = "src"
     if 'TEST_DIRS' in os.environ:
         test_dirs = os.environ['TEST_DIRS']
